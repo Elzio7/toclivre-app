4001b526849cc941cd72a3c018699418cce1aacd
Elzio7 <elzio_munhoz@hotmail.com>
2026-03-10 21:46:53 -0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,10 @@
 import React from "https://esm.sh/react@18";
 import ReactDOM from "https://esm.sh/react-dom@18/client";
+import htm from "https://esm.sh/htm@3.1.1/react";
 import App from "./App.jsx";
 
+const html = htm.bind(React.createElement);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  html`<${App} />`
 );
